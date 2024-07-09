8268d919dfe7e0af545d51239a0c23d795d74700
vamsi422 <vamshikrishna7942@gmail.com>
2024-07-09 11:18:58 +0530

    coded main.go and created authRouter and userRouter

--- golang-jwt-project/main.go
+++ golang-jwt-project/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	routes "./routes/"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	fmt.Println("welcome to golang-jwt-project.")
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8000"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+
+	routes.AuthRouters(router)
+	routes.UserRoutes(router)
+
+	router.GET("/API-1", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{"success": "Access granted for API-1"})
+	})
+	router.GET("/API-2", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{"success": "Access granted for API-2"})
+	})
+	router.Run(":" + port)
 }
