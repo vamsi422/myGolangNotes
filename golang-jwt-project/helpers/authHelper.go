d30cd9a8d8c9fb510d18f8bc1e5a3327a905d453
vamsi422 <vamshikrishna7942@gmail.com>
2024-07-12 23:26:36 +0530

    created database connection and autherisation helper

---
+++ golang-jwt-project/helpers/authHelper.go
@@ -0,0 +1,24 @@
+package helpers
+
+import (
+	"errors"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CheckUserType(c *gin.Context, role string) (err error) {
+	userType := c.GetString("user_type")
+	if userType != role {
+		return errors.New("Unauthorized to access this resource")
+	}
+	return nil
+}
+
+func MatchUserTypeToUid(c *gin.Context, userId string) (err error) {
+	userType := c.GetString("user_type")
+	uid := c.GetString("uid")
+	if userType == "USER" && uid != userId {
+		return errors.New("Unauthorized to access this resource")
+	}
+	return CheckUserType(c, userType)
+}
